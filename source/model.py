d80ba0c1b43cec286596cd6e74e76876d986500c
Arshad Kazi <kaziarshad97@gmail.com>
2023-04-10 18:21:00 +0530

    3d base model

---
+++ source/model.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv3D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class Down3D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv3D(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.mpconv(x)
+
+class Up3D(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose3d(in_channels, in_channels//2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv3D(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1    = self.up(x1)
+        diffZ = x2.size()[2] - x1.size()[2]
+        diffY = x2.size()[3] - x1.size()[3]
+        diffX = x2.size()[4] - x1.size()[4]
+        x1    = F.pad(x1, (diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2,
+                        diffZ // 2, diffZ - diffZ // 2))
+        x     = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv3D(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv3D, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet3D(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=False):
+        super().__init__()
+        self.in_channels  = in_channels
+        self.out_channels = out_channels
+        self.bilinear     = bilinear
+
+        self.conv1    = DoubleConv3D(in_channels, 64)
+        self.down1    = Down3D(64, 128)
+        self.down2    = Down3D(128, 256)
+        self.down3    = Down3D(256, 512)
+        self.down4    = Down3D(512, 1024)
+        self.up1      = Up3D(1024, 512, bilinear)
+        self.up2      = Up3D(512, 256, bilinear)
+        self.up3      = Up3D(256, 128, bilinear)
+        self.up4      = Up3D(128, 64, bilinear)
+        self.outconv  = OutConv3D(64, out_channels)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.up1(x5, x4)
+        x7 = self.up2(x6, x3)
+        x8 = self.up3(x7, x2) 
+        x9 = self.up4(x8, x1)
+        output= self.outconv(x9)
+
+        return output
+
